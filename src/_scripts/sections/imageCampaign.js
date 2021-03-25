3c401c2b0a4e2dfd253ad400c41e0039f8cc8c95
Stefan Bowerman <stefbowerman@gmail.com>
2021-03-25 10:48:34 -0700

    Clean up image campaign styling

--- src/_scripts/sections/imageCampaign.js
+++ src/_scripts/sections/imageCampaign.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
-import Swiper, { EffectFade } from 'swiper';
+import Swiper, { EffectFade, Autoplay } from 'swiper';
 import { isTouch, clamp } from '../core/utils';
 import { getBreakpointMinWidth } from '../core/breakpoints';
 import BaseSection from './base';
 
-Swiper.use([EffectFade]);
+Swiper.use([EffectFade, Autoplay]);
+
+const classes = {
+  campaignReady: 'is-ready'
+};
 
 export default class ImageCampaignSection extends BaseSection {
   constructor(container) {
@@ -15,9 +19,8 @@ export default class ImageCampaignSection extends BaseSection {
     this.$slides = $('.swiper-slide', this.$slideshow);
     this.$prev = $('[data-prev]', this.$container);
     this.$next = $('[data-next]', this.$container);
-
-    this.initialInteractionTimeout = null;
     
+    const startTime = Date.now();
     const minReadyTime = 1000;
     const mobileSettings = window.innerWidth < getBreakpointMinWidth('md');
     const effect = isTouch() && mobileSettings ? 'slide' : 'fade';
@@ -30,10 +33,8 @@ export default class ImageCampaignSection extends BaseSection {
       effect: effect,
       simulateTouch: false,
       watchOverflow: true,
-      on: {
-        slideChangeTransitionStart: this.clearInteractionTimeout.bind(this),
-        touchStart: this.clearInteractionTimeout.bind(this),
-        touchMove: this.clearInteractionTimeout.bind(this)
+      autoplay: {
+        delay: 6000
       }
     });
 
@@ -41,36 +42,19 @@ export default class ImageCampaignSection extends BaseSection {
     this.$prev.on('click', () => this.swiper.slidePrev());
     this.$next.on('click', () => this.swiper.slideNext());
 
-    const startTime = Date.now();
-
     this.$slides.first()
       .imagesLoaded()
       .done(() => {
         const loadTime = Date.now() - startTime;
-        let readyDelay = minReadyTime - loadTime;
-        
-        readyDelay = clamp(readyDelay, 0, 9999);         
+        const readyDelay = clamp(minReadyTime - loadTime, 0, 9999);         
 
-        setTimeout(this.onCampaignReady.bind(this), readyDelay);
+        setTimeout(() => {
+          this.$campaign.addClass(classes.campaignReady);
+        }, readyDelay);
       });    
   }
 
-  setInteractionTimeout() {
-    this.initialInteractionTimeout = setTimeout(() => this.swiper.slideNext(), 7000);
-  }
-
-  clearInteractionTimeout() {
-    clearTimeout(this.initialInteractionTimeout);
-  }
-
-  onCampaignReady() {
-    this.setInteractionTimeout();
-    this.$campaign.addClass('is-ready');   
-  }
-
   onKeydown({ which }) {
-    if (!this.swiper) return;
-
     if (which === 37) {
       this.swiper.slidePrev();
     }
